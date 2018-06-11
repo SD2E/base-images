af241c5661f5b9f6337a5cd4cf6e19a90bf489a1
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-06-11 09:51:39 -0500

    Explicitly handler HTTPError in agaveutils

--- reactors/python2/reactors/agaveutils/files.py
+++ reactors/python2/reactors/agaveutils/files.py
@@ -1,15 +1,62 @@
 """
 Helper functions for common filesystem operations with Agave
 """
-import re
 import os
+import re
 import time
+from random import random
 from agavepy.agave import Agave
+from requests.exceptions import HTTPError
 
 
 PWD = os.getcwd()
 MAX_ELAPSED = 300
 MAX_RETRIES = 5
+
+
+def process_agave_httperror(http_error_object):
+
+    h = http_error_object
+    # extract HTTP response code
+    code = -1
+    try:
+        code = h.response.status_code
+        assert isinstance(code, int)
+    except Exception:
+        # we have no idea what the hell happened
+        code = 418
+
+    # extract HTTP reason
+    reason = 'UNKNOWN ERROR'
+    try:
+        reason = h.response.reason
+    except Exception:
+        pass
+
+    # Extract textual response elements
+    #
+    # agave and abaco will give json responses if the
+    # underlying service is at all capable of doing so
+    # so try to extract fields from it if we can.
+    #
+    # otherwise, return the actual text of the response
+    # in error message
+    err_msg = 'Unexpected encountered by the web service'
+    status_msg = 'error'
+    version_msg = 'unknown'
+    try:
+        j = h.response.json()
+        if 'message' in j:
+            err_msg = j['message']
+        if 'status' in j:
+            status_msg = j['status']
+        if 'version' in j:
+            version_msg = j['version']
+    except Exception:
+        err_msg = h.response.text
+
+    httperror = 'HTTPError - {}: {}; message: {}; status: {}; version: {}'
+    return httperror.format(code, reason, err_msg, status_msg, version_msg)
 
 
 def agave_mkdir(agaveClient, dirName, systemId, basePath='/',
@@ -25,6 +72,9 @@ def agave_mkdir(agaveClient, dirName, systemId, basePath='/',
         agaveClient.files.manage(systemId=systemId,
                                  body={'action': 'mkdir', 'path': dirName},
                                  filePath=basePath)
+    except HTTPError as h:
+        http_err_resp = process_agave_httperror(h)
+        raise Exception(http_err_resp)
     except Exception as e:
         raise Exception(
             "Unable to mkdir {} at {}/{}: {}".format(
@@ -49,12 +99,16 @@ def agave_download_file(agaveClient,
         try:
             rsp = agaveClient.files.download(systemId=systemId,
                                              filePath=agaveAbsolutePath)
+        except HTTPError as h:
+            http_err_resp = process_agave_httperror(h)
+            raise Exception(http_err_resp)
         except Exception as e:
             raise Exception(
-                "Catastrophic error in agave_download_file: {}".format(e))
+                "Unknown error: {}".format(e))
+
         if type(rsp) == dict:
             raise Exception(
-                "Unable to download {}".format(agaveAbsolutePath))
+                "Failed to download {}".format(agaveAbsolutePath))
         for block in rsp.iter_content(2048):
             if not block:
                 break
@@ -84,8 +138,11 @@ def agave_upload_file(agaveClient,
         agaveClient.files.importData(systemId=systemId,
                                      filePath=agaveDestPath,
                                      fileToUpload=open(uploadFile))
+    except HTTPError as h:
+        http_err_resp = process_agave_httperror(h)
+        raise Exception(http_err_resp)
     except Exception as e:
-        raise Exception("Error uploading {}: {}".format(uploadFile, e))
+        raise Exception("Unknown error uploading {}: {}".format(uploadFile, e))
 
     uploaded_filename = os.path.basename(uploadFile)
     if sync:
@@ -123,17 +180,15 @@ def wait_for_file_status(agaveClient, agaveWatchPath,
     delay = 0.150  # 300 msec
     expires = (time.time() + maxTime)
     stat = None
-    ts = time.time()
 
     while (time.time() < expires):
-        elapsed = time.time() - ts
         try:
             hist = agaveClient.files.getHistory(systemId=systemId,
                                                 filePath=agaveWatchPath)
             stat = hist[-1]['status']
             if stat in TERMINAL_STATES:
                 return True
-        except Exception as e:
+        except Exception:
             # we have to swallow this exception because status isn't available
             # until the files service picks up the task. sometimes that's
             # immediate and sometimes it's backlogged - we dont' want to fail
@@ -141,8 +196,9 @@ def wait_for_file_status(agaveClient, agaveWatchPath,
             # available since we went through the trouble of setting up
             # exponential backoff!
             pass
+
         time.sleep(delay)
-        delay = (delay * 1.1)
+        delay = (delay * (1 + random()))
 
     raise Exception(
         "Status transition for {} exceeded {} sec. Last status: {}".format(
