b86e127a37f24a13e16f092b78ac73871ab95856
Ethan Ho <eho@tacc.utexas.edu>
2021-04-16 15:35:47 -0600

    Push to sd2etest DockerHub org

--- config.mk
+++ config.mk
@@ -1,7 +1,6 @@
 PYTHON ?= python3
 AGAVE_CRED_CACHE ?= ~/.agave
-# TODO
-DOCKER_ORG ?= enho
+DOCKER_ORG ?= sd2etest
 DOCKER_REPO ?= reactors
 DOCKER_TAG ?= python3
 DOCKER_IMAGE ?= $(DOCKER_ORG)/$(DOCKER_REPO):$(DOCKER_TAG)
